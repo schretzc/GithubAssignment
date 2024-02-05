79025ac57d3daf3500fbf41a62f9ae63c8133f0a
jesslynn31 <hellokittyluvrxox@gmail.com>
2024-02-05 00:29:50 -0500

    done

--- GitSome.java
+++ GitSome.java
@@ -6,6 +6,7 @@ public class GitSome{
         System.out.println("Hi, Im Chris...");
         System.out.println("Hi, I'm not Chris, I don't exist," + 
         " you didn't read this...");
+        System.out.println("Helllooooooooooo");
     }
 
 
